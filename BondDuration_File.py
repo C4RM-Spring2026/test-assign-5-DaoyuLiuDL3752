374ad4c7f88afb96cad07b2833c3bf508b956860
DaoyuLiuDL3752 <dl3752@columbia.edu>
2026-03-01 11:25:11 -0500

    Update and rename BondDuration_File (1).py to BondDuration_File.py

---
+++ BondDuration_File.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def getBondDuration(y, face, couponRate, m, ppy = 1):
+    n=int(round(m*ppy))
+    time=np.arange(1,n+1)
+    r=y/ppy
+    c=face*couponRate/ppy
+    cf=np.full(n,c)+face*(periods==n)
+    df=(1+r)**(-periods)
+    pvcf=cf*df
+    price=pvcf.sum()
+    t=time/ppy
+    return float((t*pvcg).sum()/price)
+
+y = 0.03
+face = 2000000
+couponRate = 0.04
+m = 10
